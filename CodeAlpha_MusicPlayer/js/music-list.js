084990719f7c5647197660dfd08c283ea9d564ae
aalipak <111370811+aalipak@users.noreply.github.com>
2024-07-20 20:26:42 +0500

    Add files via upload

---
+++ CodeAlpha_MusicPlayer/js/music-list.js
@@ -0,0 +1,24 @@
+
+
+let allMusic = [
+  {
+    name: "Little Do You Know",
+    artist: "Siera",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Titanic Theme",
+    artist: "Titanic",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Sohni Dharti",
+    artist: "Pakistan",
+    img: "music-4",
+    src: "music-4"
+  },
+ 
+  
+];
